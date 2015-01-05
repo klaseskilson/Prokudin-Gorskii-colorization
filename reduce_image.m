2a4ce7c7dc19acc9523e6f8c10b0258b90d18431
Klas Eskilson <klas.eskilson@gmail.com>
2015-01-05 23:41:01 +0100

    Improve reduce_image function

--- reduce_image.m
+++ reduce_image.m
@@ -9,18 +9,17 @@ function [output] = reduce_image(input, accept_width)
 % OUTPUT
 %   output - the image
 
-    if nargin < 2
-        accept_width = 300;
-    end
-    
-    [h w l] = size(input);
-    output = input;
-    
-    if w > accept_width
-        disp(['Width is ', num2str(w), ', reducing...']);
-        % reduce size
-        output = impyramid(output, 'reduce');
-        % make function call it self recursively
-        output = reduce_image(output, accept_width);
-    end
+if nargin < 2
+    accept_width = 300;
+end
+
+[h w l] = size(input);
+output = input;
+
+if w > accept_width
+    disp(['Width is ', num2str(w), ', reducing...']);
+    % reduce size using gausian 
+    output = impyramid(output, 'reduce');
+    % make function call it self recursively
+    output = reduce_image(output, accept_width);
 end
